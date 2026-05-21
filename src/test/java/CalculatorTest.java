02eaa69fc3ac225109dc67e0a5260db4678068b6
Olga-jpg06 <bazunova0@mail.ru>
2026-05-21 15:26:44 +0300

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -23,8 +23,8 @@ class CalculatorTest {
 
     @Test
     void div() {
-        assertEquals(3, calculator.div(15, 5));   // 15 / 5 = 3
-        assertEquals(0, calculator.div(7, 0));   // деление на 0 возвращает 0
+        assertEquals(3, calculator.div(15, 5));
+        assertEquals(0, calculator.div(7, 0));
         assertEquals(12, calculator.div(144, 12));
         assertEquals(0, calculator.div(10, 0));
     }
@@ -39,41 +39,44 @@ class CalculatorTest {
 
     @Test
     void solveSystem() {
-        // Система уравнений: x=1, y=2, возвращает x+y=3
         assertEquals(3, calculator.solveSystem());
     }
 
     @Test
     void solveQuadratic() {
-        // x² - 5x + 6 = 0 => корни: 2 и 3
+        // Два корня: x² - 5x + 6 = 0 -> [2, 3]
         double[] roots1 = calculator.solveQuadratic(1, -5, 6);
         assertArrayEquals(new double[]{2.0, 3.0}, roots1, 0.0001);
 
-        // x² - 4x + 4 = 0 => корень: 2 (один корень, но в массиве)
+        // Один корень: x² - 4x + 4 = 0 -> [2]
         double[] roots2 = calculator.solveQuadratic(1, -4, 4);
         assertArrayEquals(new double[]{2.0}, roots2, 0.0001);
 
-        // x² + 1 = 0 => нет действительных корней
+        // Нет корней: x² + 1 = 0 -> []
         double[] roots3 = calculator.solveQuadratic(1, 0, 1);
         assertEquals(0, roots3.length);
 
-        // 2x + 4 = 0 (линейное) => корень: -2
+        // Линейное: 2x + 4 = 0 -> [-2]
         double[] roots4 = calculator.solveQuadratic(0, 2, 4);
         assertArrayEquals(new double[]{-2.0}, roots4, 0.0001);
 
-        // x² - 4 = 0 => корни: -2 и 2
+        // x² - 4 = 0 -> [-2, 2]
         double[] roots5 = calculator.solveQuadratic(1, 0, -4);
         assertArrayEquals(new double[]{-2.0, 2.0}, roots5, 0.0001);
     }
 
     @Test
     void solveBiquadratic() {
-        // Биквадратное уравнение: x⁴ - 5x² + 4 = 0 => корни: -2, -1, 1, 2
-        double[] roots = calculator.solveBiquadratic(1, -5, 4);
-        assertArrayEquals(new double[]{-2.0, -1.0, 1.0, 2.0}, roots, 0.0001);
+        // x⁴ - 5x² + 4 = 0 -> [-2, -1, 1, 2]
+        double[] roots1 = calculator.solveBiquadratic(1, -5, 4);
+        assertArrayEquals(new double[]{-2.0, -1.0, 1.0, 2.0}, roots1, 0.0001);
 
-        // x⁴ - 4x² = 0 => корни: -2, 0, 2
+        // x⁴ - 4x² = 0 -> [-2, 0, 2]
         double[] roots2 = calculator.solveBiquadratic(1, -4, 0);
         assertArrayEquals(new double[]{-2.0, 0.0, 2.0}, roots2, 0.0001);
+
+        // x⁴ + x² = 0 -> [0] (только 0)
+        double[] roots3 = calculator.solveBiquadratic(1, 1, 0);
+        assertArrayEquals(new double[]{0.0}, roots3, 0.0001);
     }
 }
